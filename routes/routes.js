f452f6357f8031cf7d906dbfd66d665d3ea9dfe7
Zoltán Tudlik <zoltan.tudlik@gmail.com>
2018-10-07 22:41:07 +0300

    add dummy routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/task', (req, res) => {
+    const contentType = req.headers['content-type'];
+
+    if(typeof contentType === 'undefined' || contentType !== 'application/x-www-form-urlencoded') {
+        res.sendStatus(400);
+        return;
+    }
+
+    //TODO If exists, return 409
+
+    const id = 10;
+
+    res.set('Location', `/tasks/${id}`);
+    res.status(201);
+    res.json({message: 'Task successfully created', id});
 });
 
 module.exports = router;
---
+++ routes/tasks.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+
+const task1 = {
+    id: 1,
+    name: "name1",
+    created_date: new Date(),
+    status: "ok"
+};
+
+const task2 = {
+    id: 2,
+    name: "name2",
+    created_date: new Date(),
+    status: "ok"
+};
+
+const mockTasks = [task1, task2];
+
+router.get('/', (req, res) => {
+    res.json(mockTasks);
+});
+
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const resp = mockTasks.filter(task => task.id === id);
+
+    if(resp.length === 0) {
+        res.sendStatus(404);
+    } else {
+        res.json(resp);
+    }
+});
+
+router.patch('/:id', (req, res) => {
+    const contentType = req.headers['content-type'];
+
+    if(typeof contentType === 'undefined' || contentType !== 'application/x-www-form-urlencoded') {
+        res.sendStatus(400);
+        return;
+    }
+
+    //TODO Not found
+
+    const id = 10;
+    const name = "name";
+    const created_date = new Date();
+    const status = "status";
+
+    res.status(200);
+    res.json({message: 'Task successfully updated', id, name, created_date, status});
+});
+
+router.delete('/:id', (req, res) => {
+    const id = 10;
+
+    //TODO Not found
+
+    res.status(200);
+    res.json({message: 'Task successfully updated', id});
+});
+
+module.exports = router;
